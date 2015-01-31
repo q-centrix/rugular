b74fc348f09776cfc1a6901841e70a55ff43347a
Nicholas Shook <nshook@q-centrix.com>
2015-01-31 12:16:59 -0800

    load all components before app

--- lib/rugular/tasks/build.rb
+++ lib/rugular/tasks/build.rb
@@ -96,12 +96,18 @@ module Rugular
     end
 
     def javascript_files
-      Dir.glob("src/{components,app}/**/*.module.coffee") +
-        Dir.glob("src/{components,app}/**/*.routes.coffee") +
-        Dir.glob("src/{components,app}/**/*.factory.coffee") +
-        Dir.glob("src/{components,app}/**/*.filter.coffee") +
-        Dir.glob("src/{components,app}/**/*.controller.coffee") +
-        Dir.glob("src/{components,app}/**/*.directive.coffee")
+      Dir.glob("src/components/**/*.module.coffee") +
+        Dir.glob("src/components/**/*.routes.coffee") +
+        Dir.glob("src/components/**/*.factory.coffee") +
+        Dir.glob("src/components/**/*.filter.coffee") +
+        Dir.glob("src/components/**/*.controller.coffee") +
+        Dir.glob("src/components/**/*.directive.coffee") +
+        Dir.glob("src/app/**/*.module.coffee") +
+        Dir.glob("src/app/**/*.routes.coffee") +
+        Dir.glob("src/app/**/*.factory.coffee") +
+        Dir.glob("src/app/**/*.filter.coffee") +
+        Dir.glob("src/app/**/*.controller.coffee") +
+        Dir.glob("src/app/**/*.directive.coffee")
     end
 
     def bower_css
