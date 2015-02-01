b4323b624565435a0b2ce8be258d02510347d2ee
Nicholas Shook <nshook@q-centrix.com>
2015-02-01 11:03:29 -0800

    fixed another dumb mistake

--- lib/rugular/tasks/generate/filter.rb
+++ lib/rugular/tasks/generate/filter.rb
@@ -1,7 +1,7 @@
 require_relative 'generator_base'
 
 module Rugular
-  class Directive < GeneratorBase
+  class Filter < GeneratorBase
 
     def template_files
       Dir.glob("#{lib_directory}/templates/filter/*.erb")
